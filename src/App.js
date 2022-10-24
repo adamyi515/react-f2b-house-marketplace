7ca9d56ec6eae28b6dce4132956fe4ab2edfd65d
0mni-gears515 <adamyi515@gmail.com>
2022-10-24 14:22:15 -0500

    Create all page components that will be used. Finish Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Components
+import Navbar from './components/navbar.component';
+
+// Pages
+import Explore from './pages/explore.component';
+import ForgotPassword from './pages/forgot-password.component';
+import Offers from './pages/offers.component';
+import Profile from './pages/profile.component';
+import SignIn from './pages/sign-in.component';
+import SignUp from './pages/sign-up.component';
 
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </BrowserRouter>
     </>
   );
 }
